afce81c2a21922d65b879cc349bbf86653cc7f2d
Markus <mschwal1@hs-mittweida.de>
2017-07-06 09:32:23 +0200

    JavaFX observableList stuff

--- src/main/java/de/bioforscher/plip/evaluator/Interaction.java
+++ src/main/java/de/bioforscher/plip/evaluator/Interaction.java
@@ -1,5 +1,7 @@
 package de.bioforscher.plip.evaluator;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Interaction {
     private String type;
     private int residueNumber;
@@ -7,10 +9,44 @@ public class Interaction {
     private int aceptor;
 
 
+    private final SimpleStringProperty residueNumberFX = new SimpleStringProperty("");
+    private final SimpleStringProperty donorFX = new SimpleStringProperty("");
+    private final SimpleStringProperty aceptorFX = new SimpleStringProperty("");
+    private final SimpleStringProperty typeFX = new SimpleStringProperty("");
+
+
+
     public Interaction(int residueNumber, int donor, int aceptor, String type){
         this.residueNumber = residueNumber;
         this.donor = donor;
         this.aceptor = aceptor;
         this.type = type;
+    }
+
+    public Interaction(String residueNumber, String donor, String aceptor, String type){
+        setResidueNumberFX(residueNumber);
+        setDonorFX(donor);
+        setAceptorFX(aceptor);
+        setTypeFX(type);
+    }
+
+    public Interaction(){
+        this("","","","");
+    }
+
+    public void setResidueNumberFX(String residueNumberFX) {
+        this.residueNumberFX.set(residueNumberFX);
+    }
+
+    public void setDonorFX(String donorFX) {
+        this.donorFX.set(donorFX);
+    }
+
+    public void setAceptorFX(String aceptorFX) {
+        this.aceptorFX.set(aceptorFX);
+    }
+
+    public void setTypeFX(String typeFX) {
+        this.typeFX.set(typeFX);
     }
 }
